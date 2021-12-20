bfe360f328d19ef0ac3be1246f3668d0a5df1b00
EmilFrick <emil.a.frick@gmail.com>
2021-12-20 13:19:25 +0100

    goodbye

---
+++ Never404/Never404/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Never404
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
