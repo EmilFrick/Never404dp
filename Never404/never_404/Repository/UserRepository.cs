c41442f6aded1b7e88038413edb5ede3ab581a91
Adrian Naderi <Adrian.Naderi@hotmail.se>
2021-12-29 16:11:39 +0100

    Added GetAccounType and GetMembershipType method in repo

--- Never404/never_404/Repository/UserRepository.cs
+++ Never404/never_404/Repository/UserRepository.cs
@@ -1,4 +1,7 @@
-﻿namespace never_404.Repository
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace never_404.Repository
 {
     public class UserRepository
     {
@@ -31,8 +34,20 @@
             user.FirstName = firstName;
             user.LastName = lastName;
             user.Password = pw;
+            user.MembershipType = membershipType;
 
             return user;
+        }
+        public List<string> GetMebershipType()
+        {
+            List<string> membershipType = new List<string>();
+            BankDBContext db = new BankDBContext();
+            var list = db.MembershipType.Where(x => x.MembershipTypeName != "Bank");
+            foreach (var name in list)
+            {
+                membershipType.Add(name.MembershipTypeName);
+            }
+            return membershipType;
         }
     }
 }
