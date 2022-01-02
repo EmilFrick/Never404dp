bfde50084331f34d5b18588f468616341000e582
EmilFrick <emil.a.frick@gmail.com>
2022-01-02 14:05:06 +0100

    Added Specification Membership Classes

---
+++ Never404/never_404/404BankServices/Strategies/Specification/SpecificationPlatinum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace never_404._404BankServices.Strategies.Specification
+{
+    class SpecificationPlatinum
+    {
+    }
+}
